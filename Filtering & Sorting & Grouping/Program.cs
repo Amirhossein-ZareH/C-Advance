2ee917571bf4fbd13b788c14975769f157239279
AmirHossein Zare <zare20212021@gmail.com>
2025-04-20 11:58:37 +0330

    Add files via upload

---
+++ Filtering & Sorting & Grouping/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filtering___Sorting___Grouping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList()
+            {
+                23,45.654,"amir","Reza","Hani",2234,true,"arshia",false
+            };
+
+            var list1 = arrayList.OfType<int>();
+
+            var list2 = arrayList.OfType<string>();
+
+            var list3 = arrayList.OfType<double>();
+
+            var list4 = arrayList.OfType<bool>(); 
+            
+        }
+    }
+}
