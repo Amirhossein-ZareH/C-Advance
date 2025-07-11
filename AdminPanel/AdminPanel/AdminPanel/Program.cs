942712f2af442d992db3817db92228defe33126d
AmirHossein Zare <zare20212021@gmail.com>
2025-07-11 11:13:57 +0330

    Add files via upload

---
+++ AdminPanel/AdminPanel/AdminPanel/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+//using System.Data.SqlClient;
+
+namespace AdminPanel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static void AddUser()
+        {
+    
+        }
+
+        private static string ConnectionString = "Data Source=SERVER\\SERVER;Initial Catalog=AdminPanel;Intergrated Security=true";
+        private static SqlConnection connection = new SqlConnection(ConnectionString);
+        private static SqlCommand command;
+        private static SqlDataAdapter adapter;
+        private static DataTable data;
+    }
+}
